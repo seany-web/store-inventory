7d2e365afe0e01ec672fcc14d240eef0cdf2bfee
Sean <64774860+seany-web@users.noreply.github.com>
2020-07-08 11:32:15 +1200

    Update app.py
    created product model

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from peewee import *
 
 db = SqliteDatabase('products.db')
+
+class Product(Model):
+    product_id = AutoField()
+    product_quantity = IntegerField(default=0)
+    product_price = IntegerField()
+    date_updated = DateTimeField()
+
+    class Meta:
+        database = db
